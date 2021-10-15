5643b9fbd49967806514d10cc26d4359736a697b
TTiip <2711430287@qq.com>
2021-10-15 15:17:13 +0800

    原型和原型链

--- index.js
+++
@@ -1,9 +0,0 @@
-function Test() {
-  const test = 'test'
-}
-const t = new Test()
-
-console.log(t.__proto__ === Test.prototype) // true
-console.log(Test.__proto__ === Function.prototype) // true
-console.log(Test.prototype.__proto__ === Object.prototype) // true
-
