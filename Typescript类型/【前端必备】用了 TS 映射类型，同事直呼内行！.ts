ebbcb071d90099234e1981a8a81d2b04aa5b55d0
TTiip <2711430287@qq.com>
2022-05-20 18:34:56 +0800

    feat: 【前端必备】用了 TS 映射类型，同事直呼内行！

--- Typescript类型/【前端必备】用了 TS 映射类型，同事直呼内行！.ts
+++ Typescript类型/【前端必备】用了 TS 映射类型，同事直呼内行！.ts
@@ -1,3 +1,4 @@
+// 1.=========================================================
 type User = {
 	name: string
 	age: number
@@ -34,6 +35,7 @@ type MyPartial<T> = {
 // { readonly [P in K] ?: T }
 // { -readonly [P in K] ?: T }
 
+// 2.=========================================================
 // 可以使用映射类型：
 
 type Item = {
@@ -53,3 +55,52 @@ type T3 = { [P in 'a' | 'b']: Item[P] }
 
 type T4 = { [P in keyof Item]: Item[P] }
 // --> { a: string, b: number, c: boolean }
+
+// 3.=========================================================
+
+// typescript 4.1 新增了Key Remapping
+
+// type MappedTypeWithNewKesy<T> = {
+// 	[P in keyof T as NewKeyType]: T[P]
+// }
+
+// 其中 NewKeyType 必须是 String Number Symbol 联合类型的子类型
+
+// 例如：
+type Getters<T> = {
+	[K in keyof T as `get${Capitalize<string & K>}`]: () => T[K]
+}
+
+// Capitalize<string & K>
+// 此处 K 可能是 symbol 类型，所以需要加上交叉类型 过滤非string类型的健
+type Person = {
+	name: string
+	age: number
+	address: string
+}
+
+type LazyPerson = Getters<Person>
+// 映射成以下结果
+// type LazyPerson = {
+// 	getName: () => string,
+// 	getAge: () => number,
+// 	getAddress: () => string
+// }
+
+// 4.=========================================================
+
+type RemoveKindField<T> = {
+	[K in keyof T as MyExclude<K, 'kind'>]: T[K]
+}
+
+// 健重新映射是 as 子句 返回 never 类型，该健将会被删除
+// T extends U 可以简单的理解为 T 里面 包不包含 U 的健
+type MyExclude<T, U> = T extends U ? never : T
+type aa = MyExclude<keyof Circle, 'kind'>
+
+type Circle = {
+	kind: 'circle'
+	radius: number
+}
+
+type RemoveRes = RemoveKindField<Circle>
