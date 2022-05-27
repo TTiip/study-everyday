4941ac33a8b1765e4641b215b6345dba9b63a285
TTiip <2711430287@qq.com>
2022-05-27 15:58:26 +0800

    feat:【前端必备】TS 内置工具类型中的 keyof 操作符有啥用？

---
+++ Typescript类型/【前端必备】TS 内置工具类型中的 keyof 操作符有啥用？.ts
@@ -0,0 +1,76 @@
+// javascript 中我们可以使用 Object.keys 来获取对象的 健，返回的是健组成的数组。
+
+// const user = {
+// 	id: 666,
+// 	name: '张三'
+// }
+
+// Object.keys(user)
+
+// Typescript 中
+
+// 获取 type interface 等类型，获取类型的健 使用 keyof
+
+type KeyofType = {
+	id: number,
+	name: string
+}
+
+// 其返回类型是联合 “健” 组成的联合类型
+type keys = keyof KeyofType
+
+// 使用类似于对象取值的语法进行值得获取。
+type Utest1 = KeyofType['id']
+type Utest2 = KeyofType['id' | 'name']
+
+type Utest3 = KeyofType[keyof KeyofType]
+
+// 实际工作中一般用于以下情景
+// ！！！
+// js 写法
+
+// const usera = {
+// 	id: 666,
+// 	name: '张三'
+// }
+
+// function getProperty (obj, key) {
+// 	return obj[key]
+// }
+
+// getProperty(usera, 'name')
+
+// ts写法
+
+const usera = {
+	id: 666,
+	name: '张三'
+}
+
+function getProperty<T extends object, K extends keyof T> (obj: T, key: K) {
+	return obj[key]
+}
+
+getProperty(usera, 'id')
+getProperty(usera, 'name')
+getProperty(usera, 'age')
+
+// 上述情况使用
+// T extends UseraType 约束参数 obj 的类型 为 object 的类型或者其子类型
+// K extends T 约束参数 key 的类型
+// 当不存在 key 的时候，会报错
+
+// 不仅可以运用在 对象类型 以下情况也是可以的
+
+type K1 = keyof boolean // "valueOf"
+type K2 = keyof number  // "toString" | "toFixed" | "toExponential" | "toPrecision" | "valueOf" | "toLocaleString"
+type K3 = keyof any     // string | number | symbol
+
+enum HttpMethod {
+	GET,
+	POST,
+	PUT,
+	DELETE
+}
+
+type HttpMethodKeys = keyof typeof HttpMethod // "GET" | "POST" | "PUT" | "DELETE"
